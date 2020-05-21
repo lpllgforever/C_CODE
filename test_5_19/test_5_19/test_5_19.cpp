292fb634afdef81764eee7d0d332babc3ec21406
lpllgforever <747963384@qq.com>
2020-05-21 19:51:31 +0800

    learning c_code

---
+++ test_5_19/test_5_19/test_5_19.cpp
@@ -0,0 +1,50 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+//int main()
+//{
+//	/*printf("%d\n", sizeof(char));
+//	printf("%d\n", sizeof(short));
+//	printf("%d\n", sizeof(int));
+//	printf("%d\n", sizeof(long));
+//	printf("%d\n", sizeof(long long));
+//	printf("%d\n", sizeof(float));
+//	printf("%d\n", sizeof(double));*/
+//	double salary = 10000.0;
+//	return 0;
+//}
+//int a = 10;
+//int main()
+//{
+//	int a = 20;
+//	{
+//		printf("1--a=%d\n", a);
+//	}
+//	printf("2--a=%d\n", a);
+//	return 0;
+//}
+//int a = 1000;
+//int main()
+//{
+//	int a = 10;
+//	printf("a=%d\n", a);
+//	return 0;
+//}
+//int main()
+//{
+//	int num1 = 0;
+//	int num2 = 0;
+//	int sum = 0;
+//	printf("请输入两个加数：>");
+//	scanf("%d%d",&num1, &num2);
+//    sum = num1 + num2;
+//	printf("%d\n", sum);
+//
+//	return 0;
+//}
+extern int val;
+int main()
+{
+	printf("val=%d", val);
+	return 0;
+}
